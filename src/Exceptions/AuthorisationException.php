48cbc88d851866e507d916b43c83042d3c6f7bec
Abi <abi@Abis-MacBook-Air.local>
2021-05-08 19:17:03 +0100

    update

---
+++ src/Exceptions/AuthorisationException.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Abix\DataFiltering\Exceptions;
+
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class AuthorisationException extends Exception
+{
+    /**
+     * Message
+     *
+     * @var string
+     */
+    protected $message;
+
+    /**
+     * Status code
+     *
+     * @var int
+     */
+    protected $code;
+
+    /**
+     * Constructor
+     *
+     * @param string|null  $message
+     * @param integer|null $code
+     */
+    public function __construct(
+        ?string $message = null,
+        ?int $code = 403,
+        ?string $view = 'error.error'
+    ) {
+        $this->message = $message;
+        $this->code = $code;
+        $this->view = $view;
+    }
+
+    /**
+     * render the request
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function render(Request $request)
+    {
+        $message = $this->getErrorMessage($this->message);
+
+        if (!$request->acceptsJson()) {
+            abort_if(!$this->view, $this->code, $message);
+            return view($this->view)
+                ->withCode($this->code)
+                ->withMessage($message);
+        }
+
+        return response([
+            'code' => $this->code,
+            'status' => 'error',
+            'message' => $message,
+        ], $this->code);
+    }
+
+    /**
+     * Gets the status text
+     *
+     * @param  string $message
+     * @return string
+     */
+    public function getErrorMessage(?string $message = null): string
+    {
+        if ($message) {
+            return $message;
+        }
+
+        return Response::$statusTexts[$this->code];
+    }
+}
---
+++ src/Services/Authorisation/BaseAuthorisationService.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Abix\DataFiltering\Authorisation;
+
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Hash;
+use Abix\DataFiltering\Exceptions\AuthorisationException;
+
+class BaseAuthorisationService
+{
+    /**
+     * Gets the value as boolean
+     * If this is set to true, the service will not throw the exception
+     *
+     * @var bool
+     */
+    protected $resultAsBoolean = false;
+
+    /**
+     * All Errors
+     *
+     * @var array
+     */
+    public $errors = [];
+
+    /**
+     * User
+     *
+     * @var User
+     */
+    protected $authenticatable = null;
+
+    /**
+     * Gets the results as a boolean
+     *
+     * @return self
+     */
+    public function resultAsBoolean()
+    {
+        $this->resultAsBoolean = true;
+
+        return $this;
+    }
+
+    /**
+     * Sets the model to authenticate
+     *
+     * @param User $authenticatable
+     * @return self
+     */
+    public function setAuthenticatable(?User $authenticatable)
+    {
+        $this->authenticatable = $authenticatable;
+
+        return $this;
+    }
+
+    /**
+     * Verifies that the authenticatable model has the correct password
+     *
+     * @param string $password
+     * @return self
+     */
+    public function passwordVerification(string $password): self
+    {
+        if (!Hash::check($password, optional($this->authenticatable)->password)) {
+            $this->error('The provided credentials are incorrect.');
+        }
+
+        return $this;
+    }
+
+    /**
+     * Gets all the current errors
+     *
+     * @return array
+     */
+    public function getErrors(): array
+    {
+        return $this->errors;
+    }
+
+    /**
+     * Checks the validation has passed
+     *
+     * @return boolean
+     */
+    public function isValid(): bool
+    {
+        return (bool) count($this->errors);
+    }
+
+    /**
+     * Throws the exception or adds the error to the bag
+     *
+     * @param string $message
+     * @throws AuthorisationException
+     * @return void
+     */
+    protected function error(string $message): void
+    {
+        if ($this->resultAsBoolean) {
+            $this->errors[] = $message;
+
+            return;
+        }
+
+        throw new AuthorisationException($message);
+    }
+}
